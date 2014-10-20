8b40266f193b924a588abe5a0c545491b5c3a4a3
Timmy00274672 <Timmy00274672@gmail.com>
2014-10-20 15:39:38 +0800

    Show how to copy array
    Show that System.arraycopy(Object, int, Object, int, int)
     copies array (only with reference). It may throw  IndexOutOfBoundsException.

---
+++ TimmyNote/src/arrays/CopyingArrays.java
@@ -0,0 +1,40 @@
+package arrays;
+
+import java.util.Arrays;
+
+import util.CollectionData;
+import util.RandomGenerators;
+
+/**
+ * Show that {@link System#arraycopy(Object, int, Object, int, int)} copies
+ * array (only with reference). It may throw {@link IndexOutOfBoundsException}.
+ * 
+ * @author timmy00274672
+ * 
+ */
+public class CopyingArrays {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	String[] stringArray1 = CollectionData.arrays(String.class,
+		new RandomGenerators.StringGenerator(), 5);
+	/*
+	 * get java.lang.ArrayIndexOutOfBoundsException
+	 */
+	// String[] stringArray2 = new String[3];
+	String[] stringArray2 = new String[stringArray1.length];
+	System.arraycopy(stringArray1, 0, stringArray2, 0, stringArray1.length);
+	System.out.println(String.format("Array1: %s",
+		Arrays.toString(stringArray1)));
+	System.out.println(String.format("Array2: %s",
+		Arrays.toString(stringArray2)));
+
+	// Just copy the reference
+	System.out.println(String.format(
+		"stringArray1[0] == stringArray2[1] = %b",
+		stringArray1[0] == stringArray2[1]));
+    }
+
+}
