d3e2344b3d94857866051c8d5213a1cc801df4b2
Timmy00274672 <Timmy00274672@gmail.com>
2014-10-24 15:34:10 +0800

    Chage the demo code into JUnit code

---
+++ TimmyNote/test/util/map/AbstractMapTest.java
@@ -0,0 +1,156 @@
+package util.map;
+
+import java.util.AbstractMap;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import junit.framework.TestCase;
+import util.CountingMapData;
+
+public class AbstractMapTest extends TestCase {
+
+    protected AbstractMap<Integer, String> map, emptyMap;
+
+    protected void setUp() throws Exception {
+	super.setUp();
+	map = new LinkedHashMap<>();
+	emptyMap = new LinkedHashMap<>();
+	map.putAll(new CountingMapData(25));
+    }
+
+    /*
+     * public void testHashCode() { fail("Not yet implemented"); }
+     */
+    public void testEquals() {
+	HashMap<Object, Object> hashMap = new HashMap<>();
+	hashMap.putAll(map);
+	assertEquals("Equals is based on entrySet", map.equals(hashMap), true);
+	assertEquals("Equals is based on entrySet", hashMap.equals(map), true);
+    }
+
+    public void testSize() {
+	assertEquals(map.size(), 25);
+    }
+
+    public void testIsEmpty() {
+	assertEquals(map.isEmpty(), false);
+	assertEquals(emptyMap.isEmpty(), true);
+    }
+
+    public void testContainsValue() {
+	emptyMap.put(1, "A");
+	assertEquals(emptyMap.containsValue("A"), true);
+	assertEquals(emptyMap.containsValue(map), false);
+	assertEquals(emptyMap.containsValue(new String("A")), true);
+	assertEquals(emptyMap.containsKey("B"), false);
+    }
+
+    public void testContainsKey() {
+	emptyMap.put(1, "A");
+	assertEquals(emptyMap.containsKey(1), true);
+	assertEquals(emptyMap.containsKey(2), false);
+	assertEquals(emptyMap.containsKey("A"), false);
+    }
+
+    public void testGet() {
+	emptyMap.put(1, "A");
+	assertEquals(emptyMap.get(1), "A");
+	assertEquals(emptyMap.get(2), null);
+    }
+
+    public void testPut() {
+	emptyMap.put(1, "A");
+	Set<Entry<Integer, String>> keySet = emptyMap.entrySet();
+	Entry<Integer, String> next = keySet.iterator().next();
+	assertEquals(next.getValue(), "A");
+	assertEquals(next.getKey(), new Integer(1));
+    }
+
+    public void testRemove() {
+	emptyMap.put(1, "A");
+	assertEquals(emptyMap.remove(1), "A");
+	assertEquals(emptyMap.remove(1), null);
+    }
+
+    public void testPutAll() {
+	assertEquals("PutAllError", map.size(), 25);
+    }
+
+    public void testClear() {
+	map.clear();
+	assertEquals(map.size(), 0);
+    }
+
+    public void testKeySet() {
+	emptyMap.put(1, "A");
+	Set<Integer> keySet = emptyMap.keySet();
+	assertEquals(keySet.contains(1), true);
+
+	emptyMap.clear();
+	assertEquals(keySet.contains(1), false);
+
+	emptyMap.put(1, "A");
+	assertEquals(keySet.contains(1), true);
+
+	emptyMap.putAll(map);
+	assertEquals(keySet.size(), 25);
+
+	Exception exception = null;
+	try {
+	    keySet.add(1);
+	} catch (Exception e) {
+	    exception = e;
+	}
+	assertNotNull(exception);
+    }
+
+    public void testValues() {
+	emptyMap.put(1, "A");
+	Collection<String> values = emptyMap.values();
+	assertEquals(values.contains("A"), true);
+	assertEquals(values.contains("B"), false);
+	assertEquals(values.contains(1), false);
+
+	emptyMap.put(2, "B");
+	assertEquals(values.contains("B"), true);
+	values.remove(values.iterator().next());
+	assertEquals(emptyMap.size(), 1);
+
+	values.remove(values.iterator().next());
+	assertEquals(emptyMap.size(), 0);
+
+    }
+
+    public void testEntrySet() {
+	emptyMap.put(1, "A");
+	Set<Entry<Integer, String>> entrySet = emptyMap.entrySet();
+	assertEquals(entrySet.size(), 1);
+	Entry<Integer, String> first = entrySet.iterator().next();
+	assertEquals(first.getKey(), new Integer(1));
+	assertEquals(first.getValue(), "A");
+	first.setValue("B");
+	assertEquals(emptyMap.containsValue("B"), true);
+	assertEquals(emptyMap.containsValue("A"), false);
+	
+	emptyMap.put(2, "B");
+	emptyMap.put(3, "C");
+	assertEquals(entrySet.size(), 3);
+	
+	emptyMap.remove(1);
+	assertEquals(entrySet.size(), 2);
+	assertFalse(entrySet.remove(first));
+	
+	first = entrySet.iterator().next();
+	assertTrue(entrySet.remove(first));
+	assertFalse(entrySet.remove(first));
+	assertEquals(entrySet.size(), 1);
+	
+	emptyMap.putAll(map);
+	entrySet.removeAll(entrySet);
+	assertEquals(entrySet.size(), 0);
+    }
+
+}
